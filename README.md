556cc448b222ca00728310b29a84b7d1e88f0361
Diego Valdez <valdezfdiego@gmail.com>
2022-05-06 17:00:12 -0700

    Added more

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ This is my discord bot for server.
 Default prefix is: --
 ## Commands / Usage
 
-**--cprefix** *prefix* : Changes the command prefix to *prefix*. This is persistant so last prefix it was changed to will be required to change it again or use other commands.
+**prefix?**: Saying this exactly will have the bot tell the current prefix it requires.
 
-**--nrrole** *role_id* *desired_response*: will make the bot respond to anyone with the role id with the givin response.
+**--newprefix** *prefix* : Changes the command prefix to *prefix*. This is persistant so last prefix it was changed to will be required to change it again or use other commands.
+
+**--newrole** *role_id* *"desired_response"*: will make the bot respond with the response to anyone with the role id. Will overwrite previous response if same role is used. Make sure the desired_response is in quotes if more than one word.
+
+**--delrole** *role_id*: will delete the response for the role id and make it not happen again.
+
+~~**--newid** *person_id* *"desired_response"*:  Bot will use desired_response whenever person_id talks. Will overwrite previus response if same id is used. Make sure the desired_response is in quotes if more than one word.~~
+
+~~**--delid** *person_id*: will delete the response for the person id and make it not happen again.~~
--- main.py
+++ main.py
@@ -15,45 +15,65 @@ client = discord.Client()
 secrets = parse_secrets()
 with open("config.json", "r") as f:
     config = json.load(f)
-with open("responses.json", "r") as f:
-    try:
-        response = json.load(f)
-    except json.decoder.JSONDecodeError:
-        response = {}
+with open("response.json", "r") as f:
+    response = json.load(f)
+
 
 @client.event
 async def on_ready():
     print(f"{client.user} has been enabled.")
 
+
 @client.event
 async def on_message(message):
+    
     # to not execute code if message is sent by bot
     if message.author == client.user:
         return
-    elif message.guild.id == secrets["SERVER_ID"]:
+    elif message.guild.id != secrets["SERVER_ID"]:
         return
     
+    
     role_ids = [role.id for role in message.author.roles]
+    common_roles = [i for i in role_ids if i in response]
+    
     
     if any([i for i in [secrets["MOD_ID"], secrets["ADMIN_ID"], secrets["OWNER_ID"]] if i in role_ids]) or message.author.id == secrets["DIEGO_ID"]:
-        if message.content.startswith(f"{config['prefix']}cprefix"):
+        if message.content == "prefix?":
+            await message.channel.send(f"Current prefix: {config['prefix']}")
+        
+        if message.content.startswith(f"{config['prefix']}newprefix"):
             elements = shlex.split(message.content)
             config["prefix"] = elements[1]
             with open("config.json", "w") as f:
                 json.dump(config, f)
             await message.channel.send(f"Changed command prefix to: {config['prefix']}")
-        elif message.content.startswith(f'{config["prefix"]}nrrole'):
-            pass
-        elif message.content.startswith(f'{config["prefix"]}nrid'):
-            pass
-    
             
-    # if 971312988848001094 in [role.id for role in message.author.roles]:
-    #     await message.channel.send("Shutup.")
-    #     print(f"Silenced {message.author}")
-    # elif 971670598793179146 in [role.id for role in message.author.roles]:
-    #     await message.channel.send("You just got bonded.")
-    #     print(f"Bonded {message.author}")
+        elif message.content.startswith(f'{config["prefix"]}newrole'):
+            elements = shlex.split(message.content)
+            response[int(elements[1])] = elements[2]
+            with open("response.json", "w") as f:
+                json.dump(response, f)
+            await message.channel.send(f"Added Response to role: {message.guild.get_role(int(elements[1])).name}")
+        
+        elif message.content.startswith(f'{config["prefix"]}delrole'):
+            elements = shlex.split(message.content)
+            response.pop(int(elements[1]))
+            with open("response.json", "w") as f:
+                json.dump(response, f)
+            await message.channel.send(f"Removed response for role: {message.guild.get_role(int(elements[1])).name}")
+        
+        elif message.content.startswith(f'{config["prefix"]}newid'):
+            elements = shlex.split(message.content)
+        
+        elif message.content.startswith(f'{config["prefix"]}delid'):
+            elements = shlex.split(message.content)
+    
+    
+    if any(common_roles):
+        for role in common_roles:
+            await message.channel.send(response[role])
+    
 
 
 client.run(secrets["DISCORD_TOKEN"])
